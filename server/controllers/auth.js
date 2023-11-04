b2cb445c298d8aef2b861c3e3424bcf975531aeb
iamhaao <Haolee2002tst@gmail.com>
2023-11-04 20:46:51 +0700

    api sign in router

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,8 @@
 import User from "../models/user.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+import { response } from "express";
 export const signup = async (req, res, next) => {
   const { email, password, username } = req.body;
   const hashedPassword = await bcryptjs.hash(password, 10);
@@ -12,6 +14,26 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json("user create successfull!");
+  } catch (error) {
+    next(error);
+  }
+};
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      next(errorHandler(404, "User not found!!!!"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      next(errorHandler(401, "Invalid Password!!!"));
+    }
+    const token = jwt.sign({ id: validUser._id }, "iamhaao");
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(validUser);
   } catch (error) {
     next(error);
   }
