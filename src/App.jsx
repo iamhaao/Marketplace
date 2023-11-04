8ee096e13d48e4c680d335ba7c16dc03168e9dc2
iamhaao <Haolee2002tst@gmail.com>
2023-11-04 11:55:28 +0700

    setup-router

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
